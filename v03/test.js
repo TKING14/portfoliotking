9a6ba8b0f7e8400cb894f31294a6327945db0eae
TKING <92683009+TKING14@users.noreply.github.com>
2023-03-14 14:46:58 +0100

    V4.0.0

---
+++ v03/test.js
@@ -0,0 +1,13 @@
+const pagelist = 
+[
+  {type:"p", ammount:null, content:"Mission Accomplished!"},
+  {type:"p", ammount:null, content:"Mission Accomplished! 02"},
+  {type:"p", ammount:null, content:"Mission Accomplished! 03"},
+  {type:"p", ammount:null, content:"Mission Accomplished! 04"}
+];
+
+function print_me(){
+	console.log(pagelist.length);
+}
+
+print_me();
