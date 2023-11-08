d1a805fd459ae0ebf02d75a5609af292c976e4d3
Thijs de Koning <thijsdekoning14@gmail.com>
2023-11-08 13:39:15 +0100

    update v05

---
+++ v05/javascript/show.js
@@ -0,0 +1,23 @@
+const d = document;
+var origin;
+var target;
+var originID;
+var targetID;
+
+function show(targetID, originID){
+	target = d.getElementById(targetID);
+	origin = d.getElementById(originID);
+	target.style.display = 'unset';
+
+	origin.setAttribute("onclick", "hide('" + targetID + "', '" + originID + "')");
+}
+
+function hide(targetID, originID){
+	target = d.getElementById(targetID);
+	origin = d.getElementById(originID);
+	target.style.display = '';
+	console.log('hidden');
+
+	origin.setAttribute('onclick', "show('" + targetID + "', '" + originID + "')");
+	console.log('onclick -> show');
+}
