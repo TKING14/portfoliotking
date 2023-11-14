1b3f4f7b45383e4ec605f0f8cbb2347ed386a43b
Thijs de Koning <thijsdekoning14@gmail.com>
2023-11-14 16:43:29 +0100

    v6.0.0

--- v04/counter.js
+++
@@ -1,2 +0,0 @@
-const arr = ["Welcome to my portfolio website, the place where you can find more information about me! Developing online brands is what i love doing. High quality custom build websites to satisfy you and your customers!"
-];
